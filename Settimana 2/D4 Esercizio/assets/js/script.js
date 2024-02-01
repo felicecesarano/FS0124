e0abea29765ffadf422a6040f35ecfdfb2febfe3
felicecesarano <cesarano.fc@gmail.com>
2024-02-01 16:57:51 +0100

    Esercizi D4 JS

---
+++ Settimana 2/D4 Esercizio/assets/js/script.js
@@ -0,0 +1,143 @@
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function area(l1, l2) {
+    return l1 * l2;
+}
+
+console.log(area(2, 3));
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
+ la loro somma moltiplicata per tre.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function crazySum(num1, num2) {
+    if (num1 === num2) {
+        return (num1 + num2) * 3;
+    } else {
+        return num1 + num2; 
+    }
+}
+
+console.log(crazySum(6, 6));
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function crazyDiff(numero) {
+    let diff = Math.abs(numero - 19);
+    if (numero > 19) {
+        return diff * 3;
+    } else {
+        return diff;
+    }
+}
+
+console.log(crazyDiff(15));
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function boundary(n) {
+   return ((n >= 20 && n <= 100) || (n === 400));
+   
+}
+
+console.log(boundary(400)); 
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function epify(string) {
+    let str = ' IS ONLINE';
+    if (str.indexOf(string) === 0) {
+        return str
+    }
+    return string + str;
+}
+
+console.log(epify('EPICODE'))
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function check3and7(numero) {
+    return numero % 3 === 0 || numero % 7 === 0;
+}
+ 
+console.log(check3and7(28));
+
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function reverseString(stringa) {
+    let newStr = '';
+    for (var i = stringa.length - 1; i >= 0; i--) {
+        newStr += stringa[i];
+    }
+    return newStr;
+}
+
+console.log(reverseString('EPICODE'));
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function cutString(stringa) {
+    return stringa.slice(1, -1);
+}
+
+console.log(cutString('FELICE'))
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+function giveMeRandom(n) {
+    const random= [];
+    for (let i = 0; i < n; i++) {
+        random.push(Math.floor(Math.random() * 11));
+    } 
+    return random;
+}
+
+console.log(giveMeRandom(6));
