d7457224ba869f9b727cb50e99de7069f6e84256
felicecesarano <cesarano.fc@gmail.com>
2024-02-24 01:40:55 +0100

    ANIMAZIONE LETTERE M

--- UNIT2/Settimana 1/D5 Progetto/assets/js/script.js
+++ UNIT2/Settimana 1/D5 Progetto/assets/js/script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector("nav");
     const liGreen = document.getElementById('lastLi')
-    const scrollPositionToChangeColor = 400;
+    const position = 400;
+   
 
     window.addEventListener("scroll", () => {
         const scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
-        if (scrollPosition > scrollPositionToChangeColor) {
+        if (scrollPosition > position) {
             nav.classList.add("nav-scroll");
             liGreen.classList.add('liColor');
-            console.log(liGreen)
         } else {
             nav.classList.remove("nav-scroll");
             liGreen.classList.remove('liColor');
@@ -19,3 +19,27 @@ document.addEventListener("DOMContentLoaded", () => {
         window.scrollTo(0, 0);
     });
 });
+
+function animazione() {
+    let lettere = document.querySelectorAll('#casual > g');
+    function letteraCasuale() {
+        for (let i = 0; i < lettere.length; i++) {
+            let numeroRandom = Math.floor(Math.random() * lettere.length);
+            setTimeout(() => {
+                alternareVisibilita(lettere[numeroRandom]);
+            }, i * 400);
+        }
+        setTimeout(letteraCasuale, 1000);
+    }
+    letteraCasuale();
+}
+
+function alternareVisibilita(lettera) {
+    if (lettera.style.fill === 'black') {
+        lettera.style.fill = 'transparent';
+    } else {
+        lettera.style.fill = 'black';
+    }
+}
+
+animazione();
