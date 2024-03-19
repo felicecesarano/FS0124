d675c3faa90afc239a4aede87857985e737e9ed0
felicecesarano <cesarano.fc@gmail.com>
2024-03-19 15:03:53 +0100

    UNIT 3 SETTIMANA 1 ESERCIZIO 2

---
+++ UNIT3/Settimana 1/Esercizio-2/src/app/components/component1/component1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.scss']
+})
+export class Component1Component {
+  subtitle = 'ciao comp1';
+  description = 'comp1 works!';
+}
---
+++ UNIT3/Settimana 1/Esercizio-2/src/app/components/component2/component2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.scss']
+})
+export class Component2Component {
+  subtitle = 'ciao comp2';
+  description = 'comp2 works!';
+}
